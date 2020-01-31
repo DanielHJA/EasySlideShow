4c91ed2adeba5597d919ac40c07bc396b322f382
Daniel <daniel.hjartstrom@gmail.com>
2020-01-31 09:28:25 +0100

    Initial Commit

---
+++ EasySlideShow/EasySlideShow.h
@@ -0,0 +1,19 @@
+//
+//  EasySlideShow.h
+//  EasySlideShow
+//
+//  Created by Daniel Hjärtström on 2020-01-31.
+//  Copyright © 2020 Daniel Hjärtström. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EasySlideShow.
+FOUNDATION_EXPORT double EasySlideShowVersionNumber;
+
+//! Project version string for EasySlideShow.
+FOUNDATION_EXPORT const unsigned char EasySlideShowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasySlideShow/PublicHeader.h>
+
+
